25580cc6d6d16fbff0a589a0d660cbc81478a42f
Tony López <tony@lopezpagan.com>
2017-11-24 23:10:34 -0400

    Changes on gulpfile.js, templates with bootstrap, service worker, etc.

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,8 @@ var webserver = require('gulp-webserver');
  * Handlebars Templating
  */
 var hdb = require('handlebars');
-var data = require('./src/handlebars/hdbData');   //json data
-//var debug = require('./src/handlebars/hdbDebug'); //debug data
+var data = require('./src/dbs/hdbData');   //json data
+//var debug = require('./src/dbs/hdbDebug'); //debug data
 
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
@@ -33,57 +33,60 @@ var src = './src/';
 var dest = './dist/';
 
 gulp.task('imagemin', function() {
-    return gulp.src(src+'assets/img/*')
+    return gulp.src(src+'assets/img/**/*')
         .pipe(imagemin({
             progressive: true, 
             svgoPlugins: [{ removeViewBox: false }],
             use: [pngquant()]
         }))
-        .pipe(gulp.dest(dest+'assets/img'));
+        .pipe(gulp.dest(dest+'assets/img/'));
 });
 
 gulp.task('htmlmin', function() {
-  return gulp.src(src+'**/*.html')
+  console.log('***** HTML MIN *****');
+     
+  return gulp.src(src+'*.html')
             .pipe(htmlmin({collapseWhitespace: false}))
-            .pipe(gulp.dest(dest))
-            .pipe(livereload());
+            .pipe(gulp.dest(dest));
 });
 
-gulp.task("jsonimp", function() {
+gulp.task("dbs", function() {
     console.log('***** NEW DATA AVAILABLE *****');
     console.log(data.cards);
     console.log(data.lists);
-    
-    return gulp.src(src + 'handlebars/*.js')
-               .pipe(gulp.dest(dest))
-               .pipe(livereload());
-});
-
-gulp.task("jsimp", function() {
-    return gulp.src(src + 'lib/**/*.js')
-               .pipe(uglify('app.js'))
-               .pipe(gulp.dest(dest+'assets/js'))
-               .pipe(livereload());
 });
 
 gulp.task('sass', function() {
-    gulp.src(src+'sass/**/*.scss')
+    console.log('***** SASS *****');
+     
+    return gulp.src(src+'sass/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(dest+'assets/css'))
-        .pipe(livereload());
+        .pipe(gulp.dest(dest+'assets/css'));
+     
 });
 
 gulp.task('uglify', function() {
-    gulp.src([src + 'lib/**/*.js', src+'assets/js/**/*.js'])
+    console.log('***** UGLIFY *****');
+     
+    return gulp.src([src + 'lib/**/*.js', src+'assets/js/**/*.js'])
         .pipe(uglify('app.js'))
-        .pipe(gulp.dest(dest+'assets/js'))
-        .pipe(livereload());
+        .pipe(gulp.dest(dest+'assets/js'));
+});
+
+gulp.task('manifest', function() {
+     console.log('***** MANIFEST *****');
+     
+     return gulp.src(src+'manifest.json')
+            .pipe(htmlmin({collapseWhitespace: false}))
+            .pipe(gulp.dest(dest));
 });
 
 gulp.task('generate-service-worker', callback => {
+  console.log('***** GENERATE SERVICE WORKER *****');
+     
   swPrecache.write(path.join(dest, 'sw.js'), {
     staticFileGlobs: [
       // track and cache all files that match this pattern
@@ -93,43 +96,11 @@ gulp.task('generate-service-worker', callback => {
   }, callback);
 });
 
-gulp.task('livereload', function() {
-    return gulp.pipe(livereload());
-});
-
-gulp.task('webserver', function() {
-  gulp.src('dist')
-    .pipe(webserver({
-      host: 'localhost',
-      port: 3000,
-      livereload: true,
-      directoryListing: false,
-      open: true
-    }));
-});
-
-gulp.task('watch', function() {
-    livereload.listen();
-
-    gulp.watch(src+'**/*.html', ['htmlmin']);
-    gulp.watch(src+'**/*.handlebars', ['handlebars']);
-    gulp.watch(src+'handlebars/*.js', ['jsonimp']);
-    //gulp.watch(src+'lib/**/*.js', ['jsimp']);
-    gulp.watch(src+'sass/**/*.scss', ['sass']);
-    gulp.watch(src+'assets/js/**/*.js', ['uglify']);
-    gulp.watch(src+'assets/img/**/*', ['imagemin']);
-    gulp.watch(src + 'sw.js', ['generate-service-worker']);
-    
-    gulp.watch([src+'assets/css/style.css', src+'assets/js/*.js', src+'lib/**/*.js', src+'assets/img/**/*', src+'handlebars/*.js', src+'**/*.handlebars'],
-    function(files) {
-        livereload.changed(files)
-    });
-               
-});
-
-gulp.task('handlebars', function () {   
-    var options = require('./src/handlebars/hdbOptions');
-        //data = require('./src/handlebars/hdbData');   //json data
+gulp.task('handlebars', function () { 
+    console.log('***** COMPILE HANDLEBARS *****');
+     
+    var options = require(src+'dbs/hdbOptions');
+        //data = require(src+''dbs/hdbData');   //json data
     
     var templateData = {
         pageTitle: 'Page Title',
@@ -146,8 +117,48 @@ gulp.task('handlebars', function () {
             suffix: '-suf',*/
             extname: '.html'
         }))
-        .pipe(gulp.dest(dest))
-        .pipe(livereload());
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task('watch', function() {     
+    console.log('***** WATCH & LISTEN *****');
+     
+    livereload.listen();
+     
+    gulp.watch(src+'dbs/**/*.js', ['dbs']);
+    gulp.watch(src+'**/*.handlebars', ['handlebars']);
+    gulp.watch(src+'sass/**/*.scss', ['sass']);
+    gulp.watch(src+'lib/**/*.js', ['uglify']);
+    gulp.watch(src+'assets/js/**/*.js', ['uglify']);
+    gulp.watch(src+'assets/img/**/*', ['imagemin']);
+    gulp.watch(src+'manifest.json', ['manifest']);
+    gulp.watch([src+'*.html', src+'**/*.handlebars'], ['generate-service-worker']);
+    gulp.watch(src+'*.html', ['htmlmin']);
+    
+    gulp.watch([dest+'assets/css/style.css', dest+'assets/js/app.js', dest+'sw.js', dest+'assets/img/**/*', dest+'manifest.json', dest+'*.html'],
+    function(files) {
+        livereload.changed(files)
+    });
+               
+});
+
+gulp.task('livereload', function() {
+    console.log('***** LIVERELOAD *****');
+     
+    return gulp.pipe(livereload());
+});
+
+gulp.task('webserver', function() {
+  console.log('***** START WEBSERVER *****');
+     
+  gulp.src('dist')
+    .pipe(webserver({
+      host: 'localhost',
+      port: 3000,
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
 });
     
-gulp.task('default', ['watch', 'webserver', 'handlebars']);
+gulp.task('default', ['dbs', 'sass', 'uglify', 'imagemin', 'handlebars', 'manifest', 'generate-service-worker', 'htmlmin']);
---
+++ src/dbs/hdbData.js
@@ -0,0 +1,27 @@
+var data = {
+        cards: [
+            { name: 'Dorado' },
+            { name: 'San Juan' },
+            { name: 'Caguas' },
+            { name: 'Bayamón' },
+            { name: 'Toa Alta' },
+            { name: 'Toa Baja' },
+            { name: 'Vega Alta' },
+            { name: 'Vega Baja' }, 
+            { name: 'Manati' }, 
+            { name: 'Camuy' }, 
+            { name: 'Aguadilla' }, 
+            { name: 'Las Marías' }
+        ],
+        lists: [
+            { name: 'Ponce' },
+            { name: 'Fajardo' },
+            { name: 'Mayagüez' }, 
+            { name: 'Humacao' },
+            { name: 'Arecibo' }, 
+            { name: 'Utuado' }, 
+            { name: 'Guayama' }    
+        ]
+    }
+
+module.exports = data;
